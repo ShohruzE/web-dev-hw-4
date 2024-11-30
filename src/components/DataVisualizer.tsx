fd79c7ac89552236f2d8d1124de45a1a3a551b3e
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-11-30 15:21:33 -0500

    Initial commit

---
+++ src/components/DataVisualizer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DataVisualizer = () => {
+  return (
+    <iframe
+      title="Data Visualizer"
+      src="https://datawrapper.dwcdn.net/83rSx/1/"
+      style={{ width: "100%", height: "100%" }}
+    />
+  );
+};
+
+export default DataVisualizer;
