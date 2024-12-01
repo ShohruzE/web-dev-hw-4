ffd9b4009ace0f8e9a6cf884e610d32b932531f2
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-11-30 19:43:25 -0500

    Modify datawrapper UI

--- src/components/DataVisualizer.tsx
+++ src/components/DataVisualizer.tsx
@@ -2,7 +2,7 @@ const DataVisualizer = () => {
   return (
     <iframe
       title="Data Visualizer"
-      src="https://datawrapper.dwcdn.net/83rSx/1/"
+      src="https://datawrapper.dwcdn.net/ZP49l/1/"
       style={{ width: "100%", height: "100%" }}
     />
   );
